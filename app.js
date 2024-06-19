39bf8ea249756338592190402f3ad59eff0e702d
JohanBoDev <bohorquezjohan958@gmail.com>
2024-06-19 13:24:51 -0500

    agregue cors

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const cors = require('cors'); // Importa cors
 const app = express();
+
 const usuarioRoutes = require('./routes/usuarioRoutes');
 const rolRoutes = require('./routes/rolRoutes');
 const tutorRoutes = require('./routes/tutorRoutes');
 
 app.use(express.json());
+app.use(cors()); // Usa cors como middleware
 
 app.use('/usuarios', usuarioRoutes);
 app.use('/roles', rolRoutes);
 app.use('/tutores', tutorRoutes);
-// Otras rutas también se incluyen aquí
+// otras rutas también se incluyen aquí
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {

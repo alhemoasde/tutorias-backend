edb3a666c6598f686f18a4779dbcb0a8c60f98ff
alhemoasde <alhemoasde@gmail.com>
2024-06-21 09:43:18 -0500

    Ajuste ruta autenticacion

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const cors = require('cors'); // Importa cors
 const app = express();
-<<<<<<< HEAD
-
-=======
-const authRoutes = require('./routes/authRoutes');
->>>>>>> de6dc83b9e1cad4bd6257e2ae19811d373c8f587
 const usuarioRoutes = require('./routes/usuarioRoutes');
 const rolRoutes = require('./routes/rolRoutes');
 const tutorRoutes = require('./routes/tutorRoutes');
@@ -17,11 +12,7 @@ app.use('/auth', authRoutes);
 app.use('/usuarios', usuarioRoutes);
 app.use('/roles', rolRoutes);
 app.use('/tutores', tutorRoutes);
-<<<<<<< HEAD
 // otras rutas también se incluyen aquí
-=======
-// Incluir otras rutas aquí
->>>>>>> de6dc83b9e1cad4bd6257e2ae19811d373c8f587
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
